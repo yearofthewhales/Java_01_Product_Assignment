c01dce0b5c5cf8807e395af38aad271e24810b7d
373ins <373ins@gmail.com>
2016-11-22 03:43:56 -0600

    Delete Product_jr007.java

--- Product_jr007.java
+++
@@ -1,7 +0,0 @@
-// Create a Java class representing a Product
-// It should be based on the template here.
-// It should be named Product_<<NICKNAME + LAST THREE OF YOUR EID>>.java   (JDAWG_007.java)
-
-class Product {
-	private String sku = "jr007";
-}
