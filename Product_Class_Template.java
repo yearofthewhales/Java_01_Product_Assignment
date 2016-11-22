f11544fe285dc068bd82bf70ff402355d6745864
373ins <373ins@gmail.com>
2016-11-22 03:45:20 -0600

    Update Product_Class_Template.java

--- Product_Class_Template.java
+++ Product_Class_Template.java
@@ -1,3 +1,4 @@
 // Create a Java class representing a Product
 // It should be based on the template here.
-// It should be named Product_<<NICKNAME + LAST THREE OF YOUR EID>>.java   (JDAWG_007.java)
+// It should be named Product_<<eid>>.java   (Product_jr007.java)
+// It should have a private sku instance variable containing your eid.
